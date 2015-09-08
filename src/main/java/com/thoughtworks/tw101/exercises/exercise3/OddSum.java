da5f337edcebac2504200da36bb0c54d966e6537
Perla Villarreal <perlav@mit.edu>
2015-09-08 18:15:46 -0500

    Completed the minimum for Exercise 3

--- src/main/java/com/thoughtworks/tw101/exercises/exercise3/OddSum.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise3/OddSum.java
@@ -1,8 +1,14 @@
 package com.thoughtworks.tw101.exercises.exercise3;
 
 public class OddSum {
+    public static int sum = 0;
 
     public int of(int start, int end) {
-        return -1;
+        for(int i= start; i < end+1; i++){
+            if(i%2 == 1){
+                sum += i;
+            }
+        }
+        return sum;
     }
 }
