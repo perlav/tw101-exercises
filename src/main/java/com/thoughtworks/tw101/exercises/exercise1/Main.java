271b04b8bf2b182ae19b5476fcf802c5cc3e0444
Perla Villarreal <perlav@mit.edu>
2015-09-08 00:39:53 -0500

    Completed the minimum for Exercise 1

--- src/main/java/com/thoughtworks/tw101/exercises/exercise1/Main.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise1/Main.java
@@ -5,5 +5,19 @@ package com.thoughtworks.tw101.exercises.exercise1;
 public class Main {
     public static void main(String[] args) {
 
+        // Variable for total sum of the odd numbers in the 1-100 range.
+        int sum = 0;
+
+        //Range being considered from 1 to 100.
+        for(int i=1; i<101; i++){
+            //Use of modular arithmetic to identify odd numbers.
+            if (i%2 == 1){
+                sum += i;
+                System.out.println(i);
+            }
+        }
+
+        System.out.println("Sum:"+sum);
+
     }
 }
