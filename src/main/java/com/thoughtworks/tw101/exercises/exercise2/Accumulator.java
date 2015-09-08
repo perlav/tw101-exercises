f8324288f0b61ed69519f9bd845f8b237f992097
Perla Villarreal <perlav@mit.edu>
2015-09-08 14:38:43 -0500

    Completed the minimum for Exercise 2

--- src/main/java/com/thoughtworks/tw101/exercises/exercise2/Accumulator.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise2/Accumulator.java
@@ -1,4 +1,17 @@
 package com.thoughtworks.tw101.exercises.exercise2;
 
+// Exercise #2: Create a class that counts how many times you call the method increment() and prints out that number
+// when you call total(). Create a main method that calls increment() 5 times and then calls total().
+
 public class Accumulator {
+    public int count = 0;
+
+    public void increment() {
+        count ++;
+    }
+
+    public void total() {
+        System.out.println(count);
+
+    }
 }
