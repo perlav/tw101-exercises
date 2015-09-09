5b1d6918dcb79005fe728f115966bda7d0e8a429
Perla Villarreal <perlav@mit.edu>
2015-09-09 09:36:24 -0500

    Completed the minimum for Exercise 5

--- src/main/java/com/thoughtworks/tw101/exercises/exercise5/RectangleAverager.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise5/RectangleAverager.java
@@ -1,8 +1,13 @@
 package com.thoughtworks.tw101.exercises.exercise5;
 
 public class RectangleAverager {
+    private static int sum;
 
     public float averageArea(Rectangle[] rectangles) {
-        return 0;
+        sum = 0;
+        for (Rectangle r: rectangles){
+            sum += r.area();
+        }
+        return (float) sum/rectangles.length;
     }
 }
